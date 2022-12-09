dca1e5d9942827562c0e98e07a484a1a677109a7
jaehak26 <jack10829@naver.com>
2022-12-09 22:07:08 +0900

    Initial commit

---
+++ src/main/java/com/example/assignment_forum/AssignmentForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignment_forum/AssignmentForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
